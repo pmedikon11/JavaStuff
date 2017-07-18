472e67dcd2325e2c6c5486b60067990f5a813cf1
pmedikon11 <pmedikon11@gmail.com>
2017-07-18 08:20:37 -0500

    Create math.java

---
+++ math.java
@@ -0,0 +1,16 @@
+
+public class ArithmeticExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numberOne = 4;
+		int theBestNumber = 99;
+		numberOne = numberOne += 10;
+		numberOne--;
+		theBestNumber *= numberOne;
+		theBestNumber++;
+		System.out.println("numberOne "+ numberOne);
+		System.out.println("theBestNumber "+ theBestNumber);
+	}
+
+}
