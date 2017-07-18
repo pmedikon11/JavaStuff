ad0ef15e4bdd8a71034fffaa11280d1c4b48f62e
pmedikon11 <pmedikon11@gmail.com>
2017-07-18 08:21:55 -0500

    Create CharacterPrinter.java

---
+++ CharacterPrinter.java
@@ -0,0 +1,17 @@
+ 
+public class CharacterPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double health = 12.5;
+		int level;
+		String name;
+		name = "Mallis";
+		level = 1;
+		System.out.println("Level: " + level);
+		health = 9;
+		System.out.println("Health "+ health);
+		System.out.println("Character Name: "+ name);
+	}
+
+}
